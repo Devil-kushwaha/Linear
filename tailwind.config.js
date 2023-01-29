f4e67adc914bab669f472af269b547cfc863cd58
Deepanshu Kushwwaha <deepanshukushwaha18052004@gmail.com>
2023-01-29 23:56:50 +0530

    Comleted website just footer left

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.html"],
+  content: ["./build/*.html"],
   theme: {
+    screens :{
+      's500' : '500px',
+      'sm' : '640px',
+      's700' : '700px',
+      'md' : '768px',
+      'lg' : '1024px',
+      'xl' : '1280px',
+      '2xl' : '1536px',
+    },
     extend: {},
   },
   plugins: [],
